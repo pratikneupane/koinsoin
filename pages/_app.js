cbfdd8aff16abb03557ccc5653783d04f75e93bb
pratikneupane <pratikneupane1999@gmail.com>
2022-04-22 13:20:27 +0545

    added next/App

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import App from "next/app"
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
